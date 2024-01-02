61085c50c1b8c37b24af52f7667e93345172e80c
bruno-coulet <bruno.coulet@laplateforme.io>
2024-01-02 16:50:13 +0100

    modif calcul TVA

--- jour01/job09.py
+++ jour01/job09.py
@@ -6,31 +6,32 @@ class Produit:
 
     def CalculerPrixTTC (self):
         prixTTC = self.prixHT * (self.TVA+100) /100
-        return prixTTC
+        return round(prixTTC, 2)
     
     def afficher (self):
         return f"Le prix HT est de : {self.prixHT} £\nLa TVA est de : {self.TVA} %\nLe prix TTC est de : {self.CalculerPrixTTC()} £"
     
-# Ajouter des méthodes permettant de modifier le nom du produit et son prix. Ainsi que
-# des méthodes permettant de retourner chaque information du produit.
+# Ajouter des méthodes permettant de modifier le nom du produit et son prix.
 
     def nouveau_nom(self, nom):
         self.nom = nom
 
     def nouveau_prix(self, prix):
         self.prix = prix
-
+ 
+# Ainsi que des méthodes permettant de retourner chaque information du produit.
     def nom_produit(self):
         return f"le nom du produit est : {self.nom}"
         
     def prixHT_produit(self):
-        return f"le prix HT du produit est :({self.prixHT}"
+        return f"le prix HT du produit est :{self.prixHT}"
 
     def TVA_produit(self):
-        return self.TVA
+        return self.prixHT * self.TVA /100 
     
     def prixTTC_produit(self):
         return f"le prix TTC du produit est : {self.CalculerPrixTTC()}"
+    
 
 
 # Créer plusieurs produits et calculer leurs TVA.      
@@ -39,8 +40,7 @@ feculent = Produit("patate", 5, 5)
 vehicule = Produit("grosse_voiture", 50000, 20)
 
 
-# Modifier le nom et le prix de chaque produit et afficher en console le nouveau prix des
-# produits.
+# Modifier le nom et le prix de chaque produit 
 ustensile.nouveau_nom("cocotte")
 ustensile.nouveau_prix(100)
 
@@ -50,6 +50,8 @@ feculent.nouveau_prix(4)
 vehicule.nouveau_nom("citadine")
 vehicule.nouveau_prix(20000)
 
+
+# et afficher en console le nouveau prix des produits.
 print(ustensile.prixTTC_produit())
 print(feculent.prixTTC_produit())
 print(vehicule.prixTTC_produit())
