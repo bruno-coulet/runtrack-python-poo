ff5bd07ad018281ea83a2056777403420eee8633
bruno-coulet <bruno.coulet@laplateforme.io>
2024-01-02 11:48:12 +0100

    job02

---
+++ jour01/job02.py
@@ -0,0 +1,18 @@
+# À l’aide de la classe Operation créée au-dessus, imprimer en console la valeur des
+# attributs “nombre1” et “nombre2”.
+
+class Operation:
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+
+    def get_nombre1(self):
+        print("Le nombre1 est ",self.nombre1)
+
+    def get_nombre2(self):
+        print("Le nombre2 est ",self.nombre2)
+
+operation = Operation()
+
+operation.get_nombre1()
+operation.get_nombre2()
