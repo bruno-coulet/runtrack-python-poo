0e7dfff149f3464a013124db18c72e61f4276786
bruno-coulet <bruno.coulet@laplateforme.io>
2024-01-03 18:00:38 +0100

    modif en cours

--- jour02/job06.py
+++ jour02/job06.py
@@ -1,25 +1,34 @@
 # Créer une classe Commande avec les attributs privés, numéro de commande, liste de plats commandés et statut de la commande (en cours, terminée ou annulée).
 class Commande:
-    def __init__(self, numéro, plats, statut, addition):
+    def __init__(self, numéro):
         self.__number = numéro
-        self.__dishes = plats
-        self.__status = statut
-        self.__total = 0
-        self.__TVA = 0
+        self.__dishes = {}
+        self.__status = "en cours"
+  
 
 # Ajouter un plat à la commande
     def add_dish(self, dish, prix):
-        self.__dishes[dish] = {"prix": prix, "status": "en cours"}
+        if self.__status == "en cours":
+            self.__dishes[dish] = {"prix": prix, "status": "en cours"}
+            print("le plat ajouré est : ",dish)
 
 # Annuler une commande
     def cancel(self):
-        self.__dishes = {}
-        self.__total = 0
-        self.__TVA = 0  
+        self.__status = "annulée"
+        print("La commande est annulée")
+
+# _________________________________________________________________
+    def afficher_commande(self):
+        print le prix et les plats commandé
+        print("Le numéro de la commande est", self.__number)
+# __________________________________________________________________
+        
+
 
 # calculer le total d’une commande qui doit être en privé
-    def __set_addition(self):
-        self.__total = sum(plat["prix"] for plat in self.__dishes.values())
+    def __calculer_total(self):
+        total = sum(plat["prix"] for plat in self.__dishes.values()if plat["status"]=='en cours')
+        return total
 
 # et afficher une commande avec son total à payer.
     def __get_addition(self):
@@ -28,7 +37,7 @@ class Commande:
 
 # ainsi qu’une méthode permettant de calculer la TVA.
     def set_TVA(self):
-        self.__set_addition()
+        self.__calculer_total()
         self.__TVA = self.__total * 0.2
         return self.__TVA
 
@@ -38,6 +47,12 @@ salade = "Salade"
 steack = "Steack"
 tarte = "Tarte"
 
-table_12 = Commande(5, {salade: 10, steack: 15, tarte: 5}, "En cours", "addition")
 
-print(table_12.__get_addition())
+
+table_12 = Commande(1)
+table_12.add_dish(salade,10)
+
+table_12.cancel()
+table_12.afficher_commande()
+
+# print(table_12.__calculer_total())
